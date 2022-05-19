050654be39becbf3b9442493e83955c338186b2c
curranToir <105819375+curranToir@users.noreply.github.com>
2022-05-18 20:16:05 -0700

    Revert "Working on Menu"
    This reverts commit 5fc853d1f43ac9969b1567afe8e786c6156f069d.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function openNav() {
-    document.getElementById("mySidenav").style.height = "100%";
+    document.getElementById("mySidenav").style.width = "100%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
-    document.getElementById("mySidenav").style.height = "0%";
+    document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
   }
 
@@ -19,14 +19,5 @@ function menuHide(){
 
 function aboutHover(){
   document.getElementById("tryHard").innerHTML = "New text! NEW TEXT AND MORE WORDS!";
-}
-function careerHover(){
-  document.getElementById("tryHard").innerHTML = "New text! NEW TEXT!";
-}
-function contactHover(){
-  document.getElementById("tryHard").innerHTML = "New text! NEW TEXT AND less WORDS!";
-}
-function servicesHover(){
-  document.getElementById("tryHard").innerHTML = "New text! NEW TEXT AND medium WORDS!";
 }
 
