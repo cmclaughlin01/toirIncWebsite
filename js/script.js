04f880460bb53bbf6798a9f7da4cb5c8edc0915b
CurranMc <38260264+CurranMc@users.noreply.github.com>
2022-05-18 20:44:06 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.height = "100%";
   }
   
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
+    document.getElementById("mySidenav").style.height = "0%";
   }
 
 function menuHover(){
@@ -21,3 +21,12 @@ function aboutHover(){
   document.getElementById("tryHard").innerHTML = "New text! NEW TEXT AND MORE WORDS!";
 }
 
+function careerHover(){
+  document.getElementById("tryHard").innerHTML = "New text! NEW TEXT!";
+}
+function contactHover(){
+  document.getElementById("tryHard").innerHTML = "New text! NEW TEXT AND less WORDS!";
+}
+function servicesHover(){
+  document.getElementById("tryHard").innerHTML = "New text! NEW TEXT AND medium WORDS!";
+}
