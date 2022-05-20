8396ef38cfeaedf16c72347a8af51affd1a73cd9
DanieleToir <daniele@toirinc.com>
2022-05-19 21:32:27 -0400

    more php stuff

--- form.php
+++ form.php
@@ -1,11 +1,14 @@
 <?php
-
-if($_POST["message"]) {
-
-mail("daniele@toirinc.com", "Here is the subject line",
-
-$_POST["insert your message here"]. "From: contact@toirinc.com");
-
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$from = "contact@toirinc.com";
+$to = "daniele@toirinc.com";
+$subject = "Checking PHP mail";
+$message = "PHP mail works just fine";
+$headers = "From:" . $from;
+if(mail($to,$subject,$message, $headers)) {
+    echo "The email message was sent.";
+} else {
+    echo "The email message was not sent.";
 }
-
 ?>
