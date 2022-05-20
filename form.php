6818ebdee8b73f410966400d358538338794652f
DanieleToir <daniele@toirinc.com>
2022-05-19 22:35:32 -0700

    form expanded
    Reload sends again

--- form.php
+++ form.php
@@ -1,30 +1,27 @@
-
-<?php
-echo "<form method='post'>\n";
-echo "<input name='name'>\n";
-echo "<input name='email'>\n";
-echo "<input name='num'>\n";
-echo "<input name='comments'>\n";
-echo "\n";
-echo "\n";
-echo "\n";
-echo "<input name='submit'type='submit'>\n";
-echo "</form>";
-if ($_REQUEST['submit']){
-ini_set( 'display_errors', 1 );
-error_reporting( E_ALL );
-$from = "contact@toirinc.com";
-$to = "contact@toirinc.com";
-$subject = "Checking PHP mail";
-$message = "Name: ".$_REQUEST["name"]."\n
-            Email: ".$_REQUEST["email"]."\n
-            Number: ".$_REQUEST["num"]."\n
-            Comments: ".$_REQUEST["comments"];
-$headers = "From:" . $from;
-if(mail($to,$subject,$message, $headers)) {
-    echo "The email message was sent.";
-} else {
-    echo "The email message was not sent.";
-}
-}
-?>
+<div>
+    <?php
+    echo "<form method='post'>\n";
+    echo "<input name='name'>\n";
+    echo "<input name='email'>\n";
+    echo "<input name='num'>\n";
+    echo "<input name='comments'>\n";
+    echo "<input name='submit'type='submit'>\n";
+    echo "</form>";
+    if ($_REQUEST['email']!=""){
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "contact@toirinc.com";
+    $to = "contact@toirinc.com";
+    $subject = "Website Submission: ".$_REQUEST["name"].": ".$_REQUEST["comments"];
+    $message = "Name: ".$_REQUEST["name"]."\nEmail: ".$_REQUEST["email"]."\nNumber: ".$_REQUEST["num"]."\nComments: ".$_REQUEST["comments"];
+    $headers = "From:" . $from;
+    if(mail($to,$subject,$message, $headers)) {
+        echo "The email message was sent.";
+    } else {
+        echo "The email message was not sent.";
+    }
+    } else {
+    $_POST['email']='';
+    }
+    ?>
+</div>
