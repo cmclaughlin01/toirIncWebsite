ca79b2e7fc655bbd1cfc63561c7f2f0ce49f521d
curranToir <105819375+curranToir@users.noreply.github.com>
2022-05-20 14:07:11 -0700

    menu animation, making text responsive

--- js/script.js
+++ js/script.js
@@ -22,20 +22,42 @@ function menuHide(){
 
 function aboutHover(){
   document.getElementById("infoHeader").innerHTML = "ABOUT";
-  document.getElementById("infoMenu").innerHTML = "New text! NEW TEXT AND MORE WORDS!";
+  document.getElementById("infoMenu").innerHTML = "Learn more about ";
+  menuInfoAn();
 }
 
 function careerHover(){
   document.getElementById("infoHeader").innerHTML = "CAREER";
   document.getElementById("infoMenu").innerHTML = "New text! NEW TEXT AND MORE WORDS!";
+  menuInfoAn();
 }
 function contactHover(){
   document.getElementById("infoHeader").innerHTML = "CONTACT";
   document.getElementById("infoMenu").innerHTML = "New text! NEW TEXT AND MORE WORDS!";
+  menuInfoAn();
 }
 function servicesHover(){
   document.getElementById("infoHeader").innerHTML = "SERVICES";
   document.getElementById("infoMenu").innerHTML = "New text! NEW TEXT AND MORE WORDS!";
+  menuInfoAn();
+}
+function menuInfoAn(){
+  var text = document.getElementById('infoMenu');
+  var newDom = '';
+  var animationDelay = 6;
+
+  for(let i = 0; i < text.innerText.length; i++)
+  {
+      newDom += '<span class="textan">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+  }
+
+  text.innerHTML = newDom;
+  var length = text.children.length;
+
+  for(let i = 0; i < length; i++)
+  {
+      text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+  }
 }
 
 window.addEventListener('scroll', () => {
