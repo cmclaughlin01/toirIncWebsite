a2de235bef186bbc3c4496c2e44007c1d8a01d64
curranToir <105819375+curranToir@users.noreply.github.com>
2022-05-23 23:24:11 -0700

    stuff

--- js/script.js
+++ js/script.js
@@ -79,25 +79,3 @@ function homepage(){
 function secondWrite(i){
   document.getElementById('seconds').innerHTML = i.toString();
 }
-
-
-let didScroll = false;
-let paralaxTitles = document.querySelectorAll('.paralax-title');
-
-const scrollInProgress = () => {
-	didScroll = true
-}
-
-const raf = () => {
-	if(didScroll) {
-		paralaxTitles.forEach((element, index) => {
-			element.style.transform = "translateX("+ window.scrollY / 10 + "%)"
-		})
-		didScroll = false;
-	}
-	requestAnimationFrame(raf);
-}
-
-
-requestAnimationFrame(raf);
-window.addEventListener('scroll', scrollInProgress)
