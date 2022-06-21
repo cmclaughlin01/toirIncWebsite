3652cec41293fa6e871d8f34fddbefe0cd20545a
DanieleToir <daniele@toirinc.com>
2022-06-20 17:03:02 -0700

    fix anim

--- js/anim.js
+++ js/anim.js
@@ -8,16 +8,23 @@ function type(){
     var tl = gsap.timeline();
     
     var textTl = gsap.timeline({onComplete: () => {document.getElementById('lineEnding').classList.toggle('lineEnding')}});
-    textTl.to('.cls-6', {duration:0, stagger:0.15, fill: "#757575"})
-          .to('.cls-2', {duration:0, stagger:0.15, stroke: "#757575"}, "<")
+    textTl.to('.cls-6', {duration:0, stagger:0.15, fill: "#000000"})
+          .to('.cls-2', {duration:0, stagger:0.15, stroke: "#000000"}, "<")
           .to('.cls-2', {delay:0.15, duration:0, stagger:0.15, stroke: "none"}, "<")
-          .to('#lineEnding', {duration:0, stroke: "#757575"});
+          .to('#lineEnding', {duration:0, stroke: "#000000"});
 
     var popdownTl = gsap.timeline();
     popdownTl.from('.popDown', { scaleY:0, opacity:0})
-             .from('.searchBarLine', {opacity: 0})
+             .from('.popDownText', {opacity:0},"<90%")
+             .from('.searchBarLine', {opacity: 0},"<")
              .from('.popDown2', { scaleY:0, opacity:0})
-    
+             .from('.popDownText2', {opacity:0},"<90%")
+             .from('.popDown3', { scaleY:0, opacity:0})
+             .from('.popDownText3', {opacity:0},"<90%")
+             .from('.popDown4', { scaleY:0, opacity:0})
+             .from('.popDownText4', {opacity:0},"<90%")
+             
+             
     tl.add(textTl)
       .add(popdownTl);
 
@@ -31,10 +38,10 @@ function type(){
         animation:popdownTl,
         trigger: "#SearchBar",
         start: "top 15%",
-        end: "+=200",
+        end: "+=1000",
         scrub: 1,
         snap: {
-            snapTo:1,
+            snapTo:0.25,
             duration:0.5,
             delay:0
         },
@@ -48,7 +55,7 @@ function logos(){
     var marketingTl = gsap.timeline();
 
     marketingTl.from('.marketingAni', {scale: 3, transformOrigin: "bottom",})
-               .to('.marketingAni', {ease:"power1.inOut", xPercent:-100, x:0 }, "<");
+               .to('.marketingAni', {ease:"power1.inOut", x:-500, }, "<");
     
     ScrollTrigger.create({
         animation:marketingTl,
@@ -76,6 +83,7 @@ function logos(){
         scrub: true
         
     })
+
     gsap.from('.adsAni', {
         scrollTrigger: {
             trigger: '.adsAni',
@@ -101,106 +109,20 @@ function logos(){
             trigger: '.marketingAni',
             start: 'bottom bottom'
         }, opacity:0, duration:1.5});
+    // var tl = gsap.timeline();
+    // tl.to('.marketingText', {opacity:0},"<")
+    //   .to('.managerAni', {opacity:0},"<")
+    //   .to('.managerText', {opacity:0},"<")
+    //   .to('.adsAni', {opacity:0},"<")
+    //   .to('.adsText', {opacity:0},"<")
+
+    // ScrollTrigger.create({
+    //     animation: tl,
+    //     trigger: '.managerBlurb'
+    // });
+    
 }
 
 function contact(){
     gsap.from('.contactBackground', {duration:1, ease: "power2", y:1000, stagger:-0.5})
-}
-
-function adsPhone(){
-    gsap.from('.phoneImg', {
-        scrollTrigger: {
-            trigger: '#graphAni',
-            start: 'top 40%',
-            end: 'bottom 100%',
-            scrub: 0.5,
-            //markers: true,
-        },
-        ease: "power1",
-        y:300,
-        scale:1.5,
-        transformOrigin: "bottom"
-    })
-}
-
-function phoneText(){
-    var tl = gsap.timeline();
-
-    tl.to('.phoneText1', {color:'#333'})
-      .to('.phoneText1', {color:'#fff'})
-      .to('.phoneText2', {color:'#333'},'<')
-      .to('.phoneText2', {color:'#fff'})
-      .to('.phoneText3', {color:'#333'},'<')
-      .to('.phoneText3', {color:'#fff'});
-
-    ScrollTrigger.create({
-        animation:tl,
-        trigger: '#graphSect',
-        start: 'top 8.5%',
-        end: 'bottom top',
-        scrub: true,
-        pin: true,
-        markers: true
-    })
-    // gsap.to('.phoneText1', {
-    //     scrollTrigger: {
-    //         trigger: '.phoneText1',
-    //         start: 'top 40%',
-    //         end: 'bottom 70%',
-    //         scrub: 0.5,
-    //         markers: true,
-    //     },
-    //     color: "#333",  
-    // })
-    // gsap.to('.phoneText2', {
-    //     scrollTrigger: {
-    //         trigger: '.phoneText2',
-    //         start: 'top 50%',
-    //         end: 'bottom 80%',
-    //         scrub: 0.5,
-    //         markers: true,
-    //     },
-    //     color: "#333",  
-    // })
-    // gsap.to('.phoneText3', {
-    //     scrollTrigger: {
-    //         trigger: '.phoneText3',
-    //         start: 'top 70%',
-    //         end: 'bottom 99%',
-    //         scrub: 0.5,
-    //         //markers: true,
-    //     },
-    //     color: "#333",  
-    // })
-    // gsap.to('.phoneText1', {
-    //     scrollTrigger: {
-    //         trigger: '.phoneText1',
-    //         start: 'top 30%',
-    //         end: 'bottom 60%',
-    //         scrub: 0.5,
-    //         //markers: true,
-    //     },
-    //     color: "white",  
-    // })
-    // gsap.to('.phoneText2', {
-    //     scrollTrigger: {
-    //         trigger: '.phoneText2',
-    //         start: 'top 30%',
-    //         end: 'bottom 60%',
-    //         scrub: 0.5,
-    //         //markers: true,
-    //     },
-    //     color: "white",  
-    // })
-    // gsap.to('.phoneText3', {
-    //     scrollTrigger: {
-    //         trigger: '.phoneText3',
-    //         start: 'top 30%',
-    //         end: 'bottom 60%',
-    //         scrub: 0.5,
-    //         //markers: true,
-    //     },
-    //     color: "white",  
-    // })
-
 }
